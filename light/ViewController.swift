6e7ca91ea5f42f6cd8cb89fbffb95a098e7c8a98
HombreAmok <amokman@mail.ru>
2020-02-03 17:55:45 +0300

    implemented background color change

--- light/ViewController.swift
+++ light/ViewController.swift
@@ -10,9 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var ligthOn = false
+    
+    @IBAction func buttonPress(_ sender: UIButton) {
+        
+        ligthOn = !ligthOn
+        
+        updateUI() // добавили сюда после добавления отдельной   функции updateUI
+        
+//        if ligthOn {
+//            ligthOn = !ligthOn
+//        } else {
+//            ligthOn = !ligthOn
+//        }
+
+        
+//        if ligthOn == true
+//        {
+//            ligthOn  = false
+//        } else {
+//            ligthOn = true
+//        }
+    }
+    
+    func updateUI () { // перенесли из @IBAction func buttonPress(_ sender: UIButton)
+        
+        view.backgroundColor = ligthOn ? .white : .black // то же, что функция ниже, только тернарный оператор
+        
+//        if ligthOn {
+//            view.backgroundColor = .white
+//        } else {
+//            view.backgroundColor = UIColor.black // UIColor можно добавлять, а можно нет
+//        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateUI() // добавили сюда после добавления отдельной   функции updateUI
     }
 
 

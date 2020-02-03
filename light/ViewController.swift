0e92f7665e1bb32bd9ebcc96e9b06c17882c7006
HombreAmok <amokman@mail.ru>
2020-02-03 15:24:29 +0300

    Initial Commit

---
+++ light/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  light
+//
+//  Created by Юрий Руднев on 03/02/2020.
+//  Copyright © 2020 Юрий Руднев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
